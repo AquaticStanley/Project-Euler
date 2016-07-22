95957080167a823b72669782f13c5471f7bafafa
Tristan Fountain <tjfzr3@mst.edu>
2016-07-21 20:34:57 -0500

    Problem 1

---
+++ Project Euler/Problem 1/problem1.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <cstdio>
+
+int main()
+{
+	const int UPPER_BOUND=1000;
+	int total=0;
+
+	for(int i=0; i<UPPER_BOUND; i++)
+	{
+		if((i % 3 == 0) || (i % 5 == 0))
+		{
+			total+=i;
+		}
+	}
+
+	std::cout << "Total of all numbers below " << UPPER_BOUND << " that are multiples of 3 and 5: " << total;
+	std::getchar();
+}
